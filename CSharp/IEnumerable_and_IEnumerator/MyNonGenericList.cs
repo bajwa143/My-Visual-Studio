660686a53cdc4d373889c881af4b6003f5de5ad8
Saqib <hello_friends_1@yahoo.com>
2019-08-14 16:41:10 +0500

    Added IEnumerable_and_IEnumerator. Non Generic implementation of IEnumerable and IEnumerator

---
+++ CSharp/IEnumerable_and_IEnumerator/MyNonGenericList.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerable_and_IEnumerator
+{
+    // To create Enumerable collection we must implement IEnumerable interface
+    class MyNonGenericList : IEnumerable
+    {
+        // both are static because we want these two to access in our inner class
+        private static object[] _objects;
+        private static int noOfObjects;
+
+        // Constructor
+        public MyNonGenericList(int noOfItems)
+        {
+            // creating object array
+            noOfObjects = noOfItems;
+            _objects = new object[noOfObjects];
+
+            // Initializing our collection
+            for (int i = 0; i < noOfObjects; i++)
+            {
+                _objects[i] = i*2;
+
+            }
+        }
+
+        // We need to provide implementation of GetEnumerator() method of IEnumerable interface
+        public IEnumerator GetEnumerator()
+        {
+            return new MyListEnumertor();
+        }
+
+        /*
+         To implement IEnumerable/IEnumerable<T>, you must provide an enumerator.
+         You can do this in one of three ways:
+            • If the class is “wrapping” another collection, by returning the wrapped collection’s enumerator
+            • Via an iterator using yield return
+            • By instantiating your own IEnumerator/IEnumerator<T> implementation
+         */
+        // We are using last method to provide enumerator
+        // So we need to implement IEnumerator interface
+        private class MyListEnumertor : IEnumerator
+        {
+            /*
+             The IEnumerator interface specifies the following property and methods:
+                object Current { get; }
+                bool MoveNext();
+                void Reset();
+            */
+
+            private int _currentIndex = -1;
+
+            // Current property return current Object
+            public object Current
+            {
+                get
+                {
+                    return _objects[_currentIndex];
+                }
+            }
+
+            // MoveNext pointer move the pointer to next item
+            public bool MoveNext()
+            {
+                _currentIndex++;
+                return (_currentIndex < noOfObjects);
+            }
+
+            // Reset method to return the pointer back to before the first item in the list
+            public void Reset()
+            {
+                _currentIndex = -1;
+            }
+        }
+    }
+}
---
+++ CSharp/IEnumerable_and_IEnumerator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// IEnumerable and IEnumerator interfaces in .NET are implementations of the iterator pattern
+namespace IEnumerable_and_IEnumerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyNonGenericList getDoubles = new MyNonGenericList(20);
+
+            // we can use foreach only to step through an enumerable collection
+            // As our MyList class implements IEnemerable interface, we can use it in foreach loop
+            foreach (var i in getDoubles)
+            {
+                Console.WriteLine(i);
+            }
+
+            // When you compile your code, the compiler translates your foreach block to a 
+            // while loop like the earlier example. So, under the hood, it’ll use the 
+            // IEnumerator object returned from GetEnumerator method
+            Console.WriteLine("---------Now using GetEnumerator() method---------");
+            var iter = getDoubles.GetEnumerator();
+            while (iter.MoveNext())
+            {
+                Console.WriteLine(iter.Current);
+            }
+            // So, while you can use the foreach block on any types that implements IEnumerable,
+            // IEnumerable is not designed for the foreach block!
+
+            Console.Read();
+        }
+    }
+}
